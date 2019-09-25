932bac2aa860690319b3924f2a30d9dfb4106e37
takada.tomoki <takada.tomoki@kiroru-inc.jp>
2019-09-25 18:22:13 +0900

    メソッド名の修正・UILabelの背景をグレーに変更

--- SwiftLesson01/ViewController.swift
+++ SwiftLesson01/ViewController.swift
@@ -10,45 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var labelButton5: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     // ログを表示する。
-    @IBAction func button1(_ sender: UIButton) {
-        print("ボタン１が押されました")
+    @IBAction func pressedLogButton(_ sender: UIButton) {
+        print("pressed Button1")
     }
     
     // アラートを表示する。
-    @IBAction func button2(_ sender: UIButton) {
-        let alert = UIAlertController(title: "タイトル", message: "ボタン２が押されました", preferredStyle: UIAlertControllerStyle.alert)
+    @IBAction func pressedAlertButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "タイトル",
+                                      message: "ボタン２が押されました",
+                                      preferredStyle: UIAlertControllerStyle.alert)
         let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {(action: UIAlertAction!) -> Void in
-            print("button2 end")
+            print("pressed OK")
         })
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
     // アクションシートを表示する。
-    @IBAction func button3(_ sender: UIButton) {
-        let alert = UIAlertController(title: "タイトル", message: "ボタン３が押されました", preferredStyle:UIAlertControllerStyle.actionSheet)
+    @IBAction func pressedActionSheetButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "タイトル",
+                                      message: "ボタン３が押されました",
+                                      preferredStyle:UIAlertControllerStyle.actionSheet)
         let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {(action: UIAlertAction!) -> Void in
-            print("button3 end")
+            print("pressed OK")
         })
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
     // 別画面(View Controller)に遷移する。
-    @IBAction func button4(_ sender: UIButton) {
+    @IBAction func pressedSegueButton(_ sender: UIButton) {
         performSegue(withIdentifier: "button4", sender: nil)
     }
     
     // ラベルに文字を表示させる。
-    @IBAction func button5(_ sender: UIButton) {
-        labelButton5.text = "ボタン５が押されました。"
+    @IBAction func pressedEditLabelButton(_ sender: UIButton) {
+        textLabel.text = "ボタン５が押されました"
     }
     
 }
